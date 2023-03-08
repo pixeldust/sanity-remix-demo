a716411772a3c8390acb47fe0bb268d086390be4
Jason Williams <>
2023-03-08 19:40:56 +0000

    Hide links in footer.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <header className="border-t border-gray-100 transition-colors duration-1000 ease-in-out dark:border-gray-900">
       <div className="container mx-auto flex items-center justify-between p-4 lg:px-12">
         <Logo />
-        <div className="flex flex-1 flex-col items-end justify-end gap-2 text-sm md:flex-row md:items-center md:gap-5">
+        {/* <div className="flex flex-1 flex-col items-end justify-end gap-2 text-sm md:flex-row md:items-center md:gap-5">
           <a className="hover:text-cyan-600 dark:hover:text-cyan-200" href="/studio">
             Log in to Sanity Studio v3
           </a>
@@ -18,7 +18,7 @@ export default function Footer() {
           >
             Clone this project on GitHub
           </a>
-        </div>
+        </div> */}
       </div>
     </header>
   )
